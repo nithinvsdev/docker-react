8f99de7bd694c094653ad30dbbe917a15ccc1d6d
nithinvsdev <denithinvsops@gmail.com>
2023-10-29 23:18:00 +0530

    create a new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Im sexy
+          Im sexy and I know it
         </p>
         <a
           className="App-link"
